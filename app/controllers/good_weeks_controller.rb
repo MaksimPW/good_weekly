5326dec40d4a2dcb6a1634140707064802656b0e
MaksimPW <ma.savichev@gmail.com>
2016-12-31 00:09:08 +0300

    first_basis | init

---
+++ app/controllers/good_weeks_controller.rb
@@ -0,0 +1,19 @@
+class GoodWeeksController < ApplicationController
+  before_action :set_good_week, only: :show
+
+  def index
+    @good_weeks = GoodWeek.all
+  end
+
+  def show
+  end
+
+  private
+  def set_good_week
+    @good_week = GoodWeek.find(params[:id])
+  end
+
+  def good_week_params
+    params.fetch(:good_week, {})
+  end
+end
--- app/models/good_week.rb
+++ app/models/good_week.rb
@@ -1,3 +1,11 @@
 class GoodWeek < ApplicationRecord
   validates :monday, presence: true
+
+  def number_of_year
+    monday.cweek
+  end
+
+  def sunday
+    monday.end_of_week
+  end
 end
