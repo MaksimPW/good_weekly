9fdf509fcd7bc864e4ff635eda9b6bf976e3b8f7
MaksimPW <ma.savichev@gmail.com>
2017-01-01 06:38:52 +0300

    first_basis | week now

--- app/controllers/good_weeks_controller.rb
+++ app/controllers/good_weeks_controller.rb
@@ -1,13 +1,21 @@
 class GoodWeeksController < ApplicationController
   before_action :authenticate_user!
   before_action :set_good_week, only: :show
+  before_action :week_now, only: [:current_week, :show]
 
   def current_week
-    @good_week = GoodWeek.find_by(monday: DateTime.now.beginning_of_week.to_date)
     render :show
   end
 
+  def show
+    redirect_to current_week_path if week_now == @good_week
+  end
+
   private
+  def week_now
+    @good_week = GoodWeek.find_by(monday: DateTime.now.beginning_of_week.to_date)
+  end
+
   def set_good_week
     @good_week = GoodWeek.find(params[:id])
   end
