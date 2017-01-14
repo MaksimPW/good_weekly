b4aeec47d1055789ae9aa908aafcef479a1ca47c
MaksimPW <ma.savichev@gmail.com>
2017-01-14 14:51:30 +0300

    first_basis | rspec current_week

--- spec/controllers/good_weeks_controller_spec.rb
+++ spec/controllers/good_weeks_controller_spec.rb
@@ -4,7 +4,20 @@ RSpec.describe GoodWeeksController, type: :controller do
   let!(:user) { create(:user) }
 
   describe 'GET #current_week' do
+    context 'auth' do
+      let!(:good_week) { create(:good_week, monday: DateTime.now.beginning_of_week.to_date, user: user) }
+      before { sign_in_helper user }
 
+      it 'render template' do
+        get :current_week
+        expect(response).to render_template :show
+      end
+
+      it 'set current week' do
+        get :current_week
+        expect(assigns(:good_week).monday).to eq good_week.monday
+      end
+    end
   end
 
   describe 'GET #show' do
