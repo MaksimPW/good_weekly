8d3c45499fb709d510de678e929b1792921cf8aa
MaksimPW <ma.savichev@gmail.com>
2017-01-30 06:40:27 +0300

    person | has_and_belongs_to_many

--- app/models/good_week.rb
+++ app/models/good_week.rb
@@ -2,6 +2,7 @@ class GoodWeek < ApplicationRecord
   validates :monday, presence: true
   belongs_to :user
   has_and_belongs_to_many :books, join_table: 'books_good_weeks'
+  has_and_belongs_to_many :persons
 
   def self.find_by_number_of_year_and_week(year, week)
     monday = Date.commercial(year.to_i, week.to_i, 1)
